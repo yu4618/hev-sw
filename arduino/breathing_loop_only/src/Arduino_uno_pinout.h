02e90666823a9366b9f028b755a268f327e38a9c
gaes <antonio.fernandez.prieto@cern.ch>
2020-04-01 10:03:13 +0200

    Pinout for esp32 and arduino uno on separate files (not included in the main file yet)

---
+++ arduino/breathing_loop_only/src/Arduino_uno_pinout.h
@@ -0,0 +1,29 @@
+// arduino uno
+#include <LiquidCrystal.h>
+    // pwm pins
+const int pin_valve_in       = 11;
+const int pin_valve_out      = 6;
+const int pin_valve_scavenge = 5;
+
+    // adcs
+const int pin_p_supply       = A0;
+const int pin_p_regulated    = A1;
+const int pin_p_buffer       = A2;
+const int pin_p_inhale       = A3;
+const int pin_p_patient      = A4;
+
+    // leds
+const int pin_led_0          = 0;
+const int pin_led_1          = 1;
+const int pin_led_2          = 2;
+
+    // buzzer
+const int pin_buzzer         = 9;
+
+    // lcd
+const int pin_lcd_rs         = 3;
+const int pin_lcd_en         = 4;
+const int pin_lcd_d4         = 7;
+const int pin_lcd_d5         = 8;
+const int pin_lcd_d6         = 10;
+const int pin_lcd_d7         = 12;
