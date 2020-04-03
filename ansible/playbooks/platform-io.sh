e017f13033384705db8d87aa704769845998a4ff
Karol Hennessy <revkarol@gmail.com>
2020-04-03 14:57:02 +0200

    some more requirements

--- ansible/playbooks/platform-io.sh
+++ ansible/playbooks/platform-io.sh
@@ -3,5 +3,7 @@ python3 -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio
 # add to path /home/karol/.platformio/penv/bin
 
 pio lib install VariableTimedAction
+pio lib install ArduinoQueue
+pio lib install uCRC16Lib
 pio run -e uno -t upload
 #pio run -t nobuild -t upload
